46be4629701302b98311948f15987fba01ded0e6
ujjwal <uzalgunner@gmail.com>
2021-07-18 22:57:01 +0545

    payment controller added ,

--- resources/views/pages/checkout.blade.php
+++ resources/views/pages/checkout.blade.php
@@ -145,6 +145,7 @@ $vat = $setting->vat;
 
 						<div class="cart_buttons">
 							<button type="button" class="button cart_button_clear">All Cancel</button>
+							<a href="{{ route('payment.step') }}"  class="button cart_button_checkout">Final Step</a> 
 	
 						</div>
 					</div>
